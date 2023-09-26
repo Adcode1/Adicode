247e068b719ffe2aae40a4f43f8efd3dce02e09a
Adcode1 <113816770+Adcode1@users.noreply.github.com>
2023-09-26 16:47:13 +0530

    git commit -m heap added git push

---
+++ heap/kmost.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        priority_queue< pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<pair<int,int>>v;
+        for(auto x:mp){
+            pq.push({x.second,x.first});
+        }
+        while(pq.size()!=k){
+            pq.pop();
+        }
+        vector<int> ans;
+        while(!pq.empty()){
+            int temp=pq.top().second;
+            ans.push_back(temp);
+            pq.pop();
+        }
+        return ans;
+    }
+};
---
+++ heap/questions.cpp
@@ -0,0 +1,3 @@
+// 1-https://practice.geeksforgeeks.org/problems/heap-sort/1 //make heap
+// 2-https://leetcode.com/problems/top-k-frequent-elements/  //k most frequent
+// 3-https://practice.geeksforgeeks.org/problems/merge-k-sorted-arrays/1 // merge arrays
